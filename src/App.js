2f00cc2eaa4a00ec507109264d458032f231bab9
Barberousse93 <mobile.barberousse@gmail.com>
2023-06-10 21:18:14 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import styled from 'styled-components'
+
+const Test = styled.h1`
+  color: blue;
+`
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Test>Hello World !</Test>
+      <h2>Ready to code ?</h2>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
